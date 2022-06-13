13fe32f46626f1e342813f4041540ebd519fbbb0
oHack <toberock101@gmail.com>
2022-06-13 00:18:47 -0700

    added acid trip mdoe

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,20 @@
 "use strict";
-const btn = document.getElementById('button__flip__color--center');
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', () => {
-    console.log("hi");
+const btnMain = document.getElementById('button__flip__color--center');
+const btnAcid = document.getElementById('button__acid__trip');
+let repeater;
+btnMain === null || btnMain === void 0 ? void 0 : btnMain.addEventListener('click', () => { document.body.style.background = getColor(); });
+btnAcid === null || btnAcid === void 0 ? void 0 : btnAcid.addEventListener('click', () => {
+    document.body.style.background = getColor();
 });
+function getColor() {
+    return ("#" +
+        Math.random()
+            .toString(16)
+            .slice(2, 8));
+}
+btnAcid.onmouseover = function () { startAcidTrip(); };
+function startAcidTrip() {
+    console.log(`acid trip started`);
+    setTimeout(startAcidTrip, 100);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-const btn = document.getElementById('button__flip__color--center');
+const btnMain = document.getElementById('button__flip__color--center');
+const btnAcid = document.getElementById('button__acid__trip');
+let repeater: any;
 
-btn?.addEventListener('click', () => {
-    console.log("hi");
+btnMain?.addEventListener('click', () => {document.body.style.background = getColor(); });
+
+btnAcid?.addEventListener('click', () => {
+    document.body.style.background = getColor();
 });
+
+function getColor() {
+    return (
+        "#" +
+        Math.random() // ! Generates a hex code.
+        .toString(16) //Converts # to hexadecimal
+        .slice(2, 8) //Gets rid of 0 and .
+        );
+    }
+    
+    btnAcid!.onmouseover = function () { startAcidTrip(); };
+    function startAcidTrip() {
+        // document.body.style.background = getColor(); 
+        console.log(`acid trip started`);
+        setTimeout(startAcidTrip,100);
+    }
