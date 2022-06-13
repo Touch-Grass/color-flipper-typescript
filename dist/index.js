9a9b19e30210faa9d885862cf294104fdaea8eb5
oHack <toberock101@gmail.com>
2022-06-12 23:42:05 -0700

    init

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const btn = document.getElementById('button__flip__color--center');
+btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', () => {
+    console.log("hi");
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const btn = document.getElementById('button__flip__color--center');
+
+btn?.addEventListener('click', () => {
+    console.log("hi");
+});
